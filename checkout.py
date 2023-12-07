c3f86f17252c4efaaefa9c97d014e193bac20eba
Martina Bianchi <153067510+layman-penny@users.noreply.github.com>
2023-12-07 13:04:04 +0700

    Create checkout.py

---
+++ checkout.py
@@ -0,0 +1,8 @@
+# checkout.py
+from shopping_cart import ShoppingCart
+
+class Checkout:
+    def process_checkout(self, cart):
+        # Implement checkout logic here
+        # Validate data, update the database, etc.
+        print("Checkout processed successfully!")
